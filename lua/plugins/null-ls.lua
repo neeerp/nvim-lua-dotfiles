77476741d0d392646e3c824892bfc031804ff7a6
David Goldstein <goldstein.g.david@gmail.com>
2023-07-02 13:08:17 -0400

    Add ruff linter/code actions for python

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,40 +1,39 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"jose-elias-alvarez/null-ls.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    local null_ls = require("null-ls")
-    local code_actions = null_ls.builtins.code_actions
-    local diagnostics = null_ls.builtins.diagnostics
-    local formatting = null_ls.builtins.formatting
+		local null_ls = require("null-ls")
+		local code_actions = null_ls.builtins.code_actions
+		local diagnostics = null_ls.builtins.diagnostics
+		local formatting = null_ls.builtins.formatting
 
-    null_ls.setup({
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-      sources = {
-        code_actions.eslint_d,
-        diagnostics.eslint_d,
-        diagnostics.vale.with({
-          filetypes = { "markdown", "plaintex", "asciidoc" },
-        }),
-        formatting.stylua,
-        formatting.rustfmt,
-        formatting.prettierd,
-      },
-    })
-  end,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+		null_ls.setup({
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+			sources = {
+				code_actions.eslint_d,
+				diagnostics.eslint_d,
+				diagnostics.ruff,
+				formatting.stylua,
+				formatting.rustfmt,
+				formatting.prettierd,
+				formatting.ruff,
+			},
+		})
+	end,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 }
