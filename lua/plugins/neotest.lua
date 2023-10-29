4fe2d4802f98c71a6a2cbb9c25fa502338a643a7
David Goldstein <goldstein.g.david@gmail.com>
2023-10-28 20:51:56 -0400

    Use neotest to run tests

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,26 @@
+return {
+	"nvim-neotest/neotest",
+	event = { "BufRead" },
+	cmd = { "Neotest" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-neotest/neotest-python",
+		"antoinemadec/FixCursorHold.nvim",
+		"mfussenegger/nvim-dap",
+	},
+	config = function()
+		require("neotest").setup({
+			log_level = vim.log.levels.DEBUG,
+			adapters = {
+				require("neotest-python"),
+			},
+		})
+	end,
+	keys = {
+		{ "grt", [[<Cmd>lua require("neotest").run.run() <CR>]] },
+		{ "gdt", [[<Cmd>lua require("neotest").run.run({ strategy = "dap" }) <CR>]] },
+		{ "gat", [[<Cmd>lua require("neotest").run.run(vim.fn.expand("%")) <CR>]] },
+		{ "gst", [[<Cmd> lua require("neotest").summary.toggle() <CR>]] },
+	},
+	-- lazy = false,
+}
