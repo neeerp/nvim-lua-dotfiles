3afae8ebc43e74336e31113fff27745395a76bc5
David Goldstein <goldstein.g.david@gmail.com>
2023-10-30 10:24:16 -0400

    Use prettier for jsx and css

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -21,6 +21,10 @@ return {
 			lua = { "stylua" },
 			python = { "ruff_fix", "ruff_format" },
 			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			javascriptreact = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
+			css = { { "prettierd", "prettier" } },
 		},
 		-- Set up format-on-save
 		format_on_save = { timeout_ms = 500, lsp_fallback = true },
