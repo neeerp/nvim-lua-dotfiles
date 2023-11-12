c4a18d3f468bd8566e83d50ea654b851f48c96b1
David Goldstein <goldstein.g.david@gmail.com>
2023-11-12 08:36:37 -0500

    Update Neotest and DAP keybinds

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,12 +10,15 @@ return {
 			},
 		},
 		keys = {
-			{ "gbd", [[<Cmd>lua require("dap").toggle_breakpoint() <CR>]] },
-			{ "grd", [[<Cmd>lua require("dap").repl.open() <CR>]] },
+			{ "gbp", [[<Cmd>lua require("dap").toggle_breakpoint() <CR>]] },
+			{ "grp", [[<Cmd>lua require("dap").repl.open() <CR>]] },
 			{ "gcd", [[<Cmd>lua require("dap").continue() <CR>]] },
 			{ "gsi", [[<Cmd>lua require("dap").step_into() <CR>]] },
+			{ "<leader>si", [[<Cmd>lua require("dap").step_into() <CR>]] },
 			{ "gsu", [[<Cmd>lua require("dap").step_out() <CR>]] },
+			{ "<leader>su", [[<Cmd>lua require("dap").step_out() <CR>]] },
 			{ "gso", [[<Cmd>lua require("dap").step_over() <CR>]] },
+			{ "<leader>so", [[<Cmd>lua require("dap").step_over() <CR>]] },
 
 			-- Don't actually need this... the repl is extremely powerful
 			-- and can show us frames/scopes/whatever already
