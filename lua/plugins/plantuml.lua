ccabd276583619889f7af3188385bd2ad286fc07
David Goldstein <goldstein.g.david@gmail.com>
2023-11-12 09:20:29 -0500

    Move writing/doc preview plugins into a separate directory

--- lua/plugins/plantuml.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"weirongxu/plantuml-previewer.vim",
-	dependencies = {
-		"tyru/open-browser.vim",
-	},
-}
---
+++ lua/plugins/writing/init.lua
@@ -0,0 +1,10 @@
+return {
+	require("plugins.writing.vimtex"),
+	{
+		"weirongxu/plantuml-previewer.vim",
+		dependencies = {
+			"tyru/open-browser.vim",
+		},
+	},
+	"iamcco/markdown-preview.nvim",
+}
