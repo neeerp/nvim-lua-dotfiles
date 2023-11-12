42c0c735cca98030f7c823e35eb9da33dc061329
David Goldstein <goldstein.g.david@gmail.com>
2023-11-12 10:48:47 -0500

    Move git related plugins together

---
+++ lua/plugins/git.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		opts = {
+			max_file_length = 12000,
+		},
+	},
+	{
+		"FabijanZulj/blame.nvim",
+		config = function()
+			require("blame").setup({
+				merge_consecutive = false,
+			})
+		end,
+		keys = {
+			{ "gtb", [[<Cmd>ToggleBlame<CR>]] },
+		},
+	},
+}
