500bdf3cd5b6b0b941b11e0d69452942a38e3fb9
David Goldstein <goldstein.g.david@gmail.com>
2023-11-12 11:01:20 -0500

    Got UFO to work properly by copying from chrisgrieser/.config

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,27 +1,103 @@
+local foldIcon = ""
+local hlgroup = "NonText"
+local function foldTextFormatter(virtText, lnum, endLnum, width, truncate)
+	local newVirtText = {}
+	local suffix = "  " .. foldIcon .. "  " .. tostring(endLnum - lnum)
+	local sufWidth = vim.fn.strdisplaywidth(suffix)
+	local targetWidth = width - sufWidth
+	local curWidth = 0
+	for _, chunk in ipairs(virtText) do
+		local chunkText = chunk[1]
+		local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+		if targetWidth > curWidth + chunkWidth then
+			table.insert(newVirtText, chunk)
+		else
+			chunkText = truncate(chunkText, targetWidth - curWidth)
+			local hlGroup = chunk[2]
+			table.insert(newVirtText, { chunkText, hlGroup })
+			chunkWidth = vim.fn.strdisplaywidth(chunkText)
+			if curWidth + chunkWidth < targetWidth then
+				suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+			end
+			break
+		end
+		curWidth = curWidth + chunkWidth
+	end
+	table.insert(newVirtText, { suffix, hlgroup })
+	return newVirtText
+end
+
 return {
-  "kevinhwang91/nvim-ufo",
-  dependencies = {
-    "kevinhwang91/promise-async",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    vim.o.foldcolumn = "1" -- '0' is not bad
-    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
-
-    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-    vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-    vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-
-    -- Option 3: treesitter as a main provider instead
-    -- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
-    -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
-    require("ufo").setup({
-      provider_selector = function(bufnr, filetype, buftype)
-        return { "treesitter", "indent" }
-      end,
-    })
-    --
-  end,
+	"kevinhwang91/nvim-ufo",
+	dependencies = "kevinhwang91/promise-async",
+	event = "BufReadPost",
+	keys = {
+		{
+			"zr",
+			function()
+				require("ufo").openFoldsExceptKinds({ "comment" })
+			end,
+			desc = " 󱃄 Open All Folds except comments",
+		},
+		{
+			"zm",
+			function()
+				require("ufo").closeAllFolds()
+			end,
+			desc = " 󱃄 Close All Folds",
+		},
+		{
+			"z1",
+			function()
+				require("ufo").closeFoldsWith(1)
+			end,
+			desc = " 󱃄 Close L1 Folds",
+		},
+		{
+			"z2",
+			function()
+				require("ufo").closeFoldsWith(2)
+			end,
+			desc = " 󱃄 Close L2 Folds",
+		},
+		{
+			"z3",
+			function()
+				require("ufo").closeFoldsWith(3)
+			end,
+			desc = " 󱃄 Close L3 Folds",
+		},
+		{
+			"z4",
+			function()
+				require("ufo").closeFoldsWith(4)
+			end,
+			desc = " 󱃄 Close L4 Folds",
+		},
+	},
+	init = function()
+		-- INFO fold commands usually change the foldlevel, which fixes folds, e.g.
+		-- auto-closing them after leaving insert mode, however ufo does not seem to
+		-- have equivalents for zr and zm because there is no saved fold level.
+		-- Consequently, the vim-internal fold levels need to be disabled by setting
+		-- them to 99
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+	end,
+	opts = {
+		provider_selector = function(_, ft, _)
+			-- INFO some filetypes only allow indent, some only LSP, some only
+			-- treesitter. However, ufo only accepts two kinds as priority,
+			-- therefore making this function necessary :/
+			local lspWithOutFolding = { "markdown", "sh", "css", "html", "python" }
+			if vim.tbl_contains(lspWithOutFolding, ft) then
+				return { "treesitter", "indent" }
+			end
+			return { "lsp", "indent" }
+		end, -- open opening the buffer, close these fold kinds
+		-- use `:UfoInspect` to get available fold kinds from the LSP
+		close_fold_kinds = { "imports", "comment" },
+		open_fold_hl_timeout = 800,
+		fold_virt_text_handler = foldTextFormatter,
+	},
 }
